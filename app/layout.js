19396d6ce00301574eab18f99f0b462850e7ae4e
kennethtaylor <taylor.kennethg@gmail.com>
2023-10-05 16:31:08 -0700

    add: GA4

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import "./globals.css";
 import StyledComponentsRegistry from "../lib/registry";
 import dynamic from "next/dynamic";
+import Script from "next/script";
 
 const Header = dynamic(() => import("@/app/components/Header"), {
 	ssr: false,
@@ -13,6 +14,13 @@ const Footer = dynamic(() => import("@/app/components/Footer"), {
 export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
+			<Script src="https://www.googletagmanager.com/gtag/js?id=G-SM79XXQQQX" />
+			<Script id="wicked-ga4">
+				{`window.dataLayer = window.dataLayer || [];
+					function gtag(){dataLayer.push(arguments);}
+					gtag('js', new Date());
+					gtag('config', 'G-SM79XXQQQX');`}
+			</Script>
 			<StyledComponentsRegistry>
 				<body>
 					<Header />
