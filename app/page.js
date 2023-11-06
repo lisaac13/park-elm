36bfc1c4140a9a8afef65f677dff2ddbe4681b6b
kennethtaylor <taylor.kennethg@gmail.com>
2023-11-06 11:24:34 -0800

    og image

--- app/page.js
+++ app/page.js
@@ -3,6 +3,8 @@ import getPageBySlug from "./utils/getPageBySlug";
 import parseSections from "./utils/parseSections";
 
 export const metadata = {
+	metadataBase: new URL("https://parkelmcms.wpenginepowered.com"),
+	// metadataBase: new URL("https://localhost:3000"),
 	title: "Park Elm at Century Plaza",
 	description:
 		"Park Elm at Century Plaza strikes the perfect architectural balance — offering utmost privacy and unimpeded views of the ocean, hills and skyline. With lavish lifestyle and wellness amenities, the iconic Century Plaza at its base, and Beverly Hills moments away, Park Elm is LA’s new beacon of luxury living.",
