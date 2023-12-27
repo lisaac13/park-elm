e76751c4ad9cdd15ffe8bd661a4ca566eb09c327
kcreinawicked <61247292+kcreinawicked@users.noreply.github.com>
2023-12-27 01:06:01 -0500

    Availability

--- app/components/Availability.js
+++ app/components/Availability.js
@@ -18,6 +18,9 @@ const Availabilityrapper = styled.section`
     @media screen and (max-width: 600px) {
         &.paddingpage {padding: 12rem 2rem 6rem 2rem;}
     }
+    @media screen and (max-width: 326px) {
+        &.paddingpage {padding: 12rem 1rem 6rem 1rem;}
+    }
 `;
 const AvailabilityContent = styled.div`
     width: 100%;
@@ -26,7 +29,9 @@ const AvailabilityContent = styled.div`
     text-align: center;
     display: block;
     padding: 0 0 6rem 0;
-
+    @media screen and (max-width: 326px) {
+        padding: 0 1rem 6rem 1rem;
+    }
    & h1 {
         font-size: var(--heading);
         padding: 0 0 2rem 0;
@@ -58,6 +63,10 @@ const ResidenceContainer = styled.div`
     font-size: var(--smallheading);
     font-weight: 500;
 }
+@media screen and (max-width: 900px) {
+    & h2 {border-bottom: 1px solid var(--sky);
+    padding: 0 0 1rem 0;}
+}
 `;
 
 const Heading = styled.ul`
@@ -68,8 +77,17 @@ const Heading = styled.ul`
     gap: 1rem;
     border-bottom: 1px solid var(--sky);
     padding: 1rem 0;
+    & li.first {
+        width: calc(14% - 1rem);
+    }
+    & li.inner {
+        width: calc(17% - 1rem);
+    }
+    & li.end {
+        width: calc(18% - 1rem);
+    }
+
     & li {
-        width: calc(16.66% - 1rem);
         font-family: var(--font-sans-serif);
         text-transform: uppercase;
         font-weight: 400;
@@ -85,6 +103,9 @@ const Heading = styled.ul`
     & li.right {  
         text-align: right;
     }
+    @media screen and (max-width: 900px) {
+        display: none;
+    }
 `;
 const SingleResidence = styled.ul`
     list-style: none;
@@ -94,12 +115,17 @@ const SingleResidence = styled.ul`
     gap: 1rem;
     padding: 1rem 0;
     border-bottom: 1px solid var(--sky);
-    & li {
-        width: calc(16.66% - 1rem);
+    & li.first {
+        width: calc(14% - 1rem);
+    }
+    & li.inner {
+        width: calc(17% - 1rem);
+    }
+    & li.end {
+        width: calc(18% - 1rem);
     }
     & li.heading {
         font-family: var(--font-serif-medium);
-        text-transform: uppercase;
         font-weight: 500;
         font-size: var(--smallheading);
         color: var(--rose);
@@ -119,6 +145,54 @@ const SingleResidence = styled.ul`
     }
     & li.right {  
         text-align: right;
+    }
+    & li .mobileShowHeading,
+    & li .mobileShow {display: none;}
+    @media screen and (max-width: 900px) {
+        border-bottom: 0px solid var(--sky);
+        padding: 0rem 0 0 0;
+        gap: 0;
+        & li.first {
+            width: 100%;
+        }
+        & li.inner {
+            width: 50%;
+            border-bottom: 1px solid var(--sky);
+            padding: 1rem 0;
+            position: relative;
+        }
+        & li.inner.line::after {
+            content: "";
+            position: absolute;
+            top: 0;
+            bottom: 0;
+            right: 0;
+            background: var(--sky);
+            width: 1px;
+            height: 100%;
+            display: block;
+        }
+        & li.end {
+            width: 100%;
+        }
+        & li .mobileShowHeading,
+        & li .mobileShow {display: inline; color: var(--rose);}
+        & li.heading {
+            padding: 1rem 0;
+            border-bottom: 1px solid var(--sky);
+        }
+        & li.end {
+            background: var(--sky);
+            padding: 1rem 0;
+        }
+        & li.end.reg.right {text-align: center;}
+        & li.inner.reg.center {
+            text-align: left;
+        }
+        & li.paddLeft {padding-left: 1rem;}
+    }
+    @media screen and (max-width: 500px) {
+        & li .mobileShow {display: block;}
     }
 `;
 export const AvailabilityQueryFragment = `
@@ -182,22 +256,22 @@ export default function Availability(props) {
                         <ResidenceContainer key={index}>
                         {item.title && <h2>{item.title}</h2>}
                         <Heading>
-                            <li className="left">Residence</li>
-                            <li className="center">List Price</li>
-                            <li className="center">Bed / Bath</li>
-                            <li className="center">SF / SM</li>
-                            <li className="center">View Direction</li>
-                            <li className="rightr">Additional Information</li>
+                            <li className="first left">Residence</li>
+                            <li className="inner center">List Price</li>
+                            <li className="inner center">Bed / Bath</li>
+                            <li className="inner center">SF / SM</li>
+                            <li className="inner center">View Direction</li>
+                            <li className="end right">Additional Information</li>
                         </Heading>
                         {item?.residences?.map((single, index) => {
                             return (
                             <SingleResidence key={`single-${index}`}>
-                            <li className="heading left">{single?.singleResidences?.residence}</li>
-                            <li className="reg center">{single?.singleResidences?.price}</li>
-                            <li className="reg center">{single?.singleResidences?.bedrooms} / {single?.singleResidences?.bathrooms}</li>
-                            <li className="reg center">{single?.singleResidences?.squareFeet} / {single?.singleResidences?.squareMeters}</li>
-                            <li className="reg center">{single?.singleResidences?.viewDirection}</li>
-                            <li className="reg right">{single?.singleResidences?.floorPlan && <span>Floor Plan</span>}{single?.singleResidences?.videoEmbed && <span>Video</span>}</li>
+                            <li className="first heading left"><span className="mobileShowHeading">Redsidence </span>{single?.singleResidences?.residence}</li>
+                            <li className="inner reg center line"><span className="mobileShow">Listing Price: </span>{single?.singleResidences?.price}</li>
+                            <li className="inner reg center paddLeft"><span className="mobileShow">Bed / Bath: </span>{single?.singleResidences?.bedrooms} / {single?.singleResidences?.bathrooms}</li>
+                            <li className="inner reg center line"><span className="mobileShow">SQ FT / SQ M: </span>{single?.singleResidences?.squareFeet} / {single?.singleResidences?.squareMeters}</li>
+                            <li className="inner reg center paddLeft"><span className="mobileShow">View Direction: </span>{single?.singleResidences?.viewDirection}</li>
+                            <li className="end reg right">{single?.singleResidences?.floorPlan && <span>Floor Plan</span>}{single?.singleResidences?.videoEmbed && <span>Video</span>} <span>Inquire</span></li>
                             </SingleResidence>
                             )
                         })}
