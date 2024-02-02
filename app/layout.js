9dd011da39531b24b61add3d83c4507cf72748d5
kennethtaylor <taylor.kennethg@gmail.com>
2024-02-02 11:06:52 -0800

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,16 +1,22 @@
 "use client";
 import "./globals.css";
-import { useContext, useLayoutEffect, useRef, useEffect, useState } from "react";
+import {
+	useContext,
+	useLayoutEffect,
+	useRef,
+	useEffect,
+	useState,
+} from "react";
 import StyledComponentsRegistry from "../lib/registry";
 import dynamic from "next/dynamic";
 import Script from "next/script";
 import Head from "next/head";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import { ScrollToPlugin } from "gsap/ScrollToPlugin";
-import { useIsomorphicLayoutEffect } from '@/lib/helpers/isomorphicEffect';
-import TransitionContext from '@/lib/context/TransitionContext';
+import { useIsomorphicLayoutEffect } from "@/lib/helpers/isomorphicEffect";
+import TransitionContext from "@/lib/context/TransitionContext";
 import TrackingScripts from "@/app/components/TrackingScripts";
 import { TransitionProvider } from "@/lib/context/TransitionContext";
 import TransitionComponent from "@/app/components/Transition";
@@ -28,49 +34,65 @@ export default function RootLayout({ children }) {
 	const main = useRef();
 	const pathname = usePathname();
 	const scrollTween = useRef();
-  	const [ctx] = useState(gsap.context(() => {}, main));
-  	const { completed } = useContext(TransitionContext);
+	const [ctx] = useState(gsap.context(() => {}, main));
+	const { completed } = useContext(TransitionContext);
 
-	  const goToSection = (i) => {
+	const goToSection = (i) => {
 		// Remove the GSAP instance with the specific ID
 		// to prevent memory leak
 		ctx.data.forEach((e) => {
-		  if (e.vars && e.vars.id === 'scrollTween') {
-			e.kill();
-		  }
+			if (e.vars && e.vars.id === "scrollTween") {
+				e.kill();
+			}
 		});
 		ctx.add(() => {
-		  scrollTween.current = gsap.to(window, {
-			scrollTo: { y: i * window.innerHeight, autoKill: false },
-			duration: 1,
-			id: 'scrollTween',
-			onComplete: () => (scrollTween.current = null),
-			overwrite: true,
-		  });
+			scrollTween.current = gsap.to(window, {
+				scrollTo: { y: i * window.innerHeight, autoKill: false },
+				duration: 1,
+				id: "scrollTween",
+				onComplete: () => (scrollTween.current = null),
+				overwrite: true,
+			});
 		});
-	  };
-	
-	  useIsomorphicLayoutEffect(() => {
+	};
+
+	useIsomorphicLayoutEffect(() => {
 		if (!completed) return;
 		ctx.add(() => {
-		  const panels = gsap.utils.toArray('.panel');
-		  panels.forEach((panel, i) => {
-			ScrollTrigger.create({
-			  trigger: panel,
-			  start: 'top bottom',
-			  end: '+=200%',
-			  onToggle: (self) =>
-				self.isActive && !scrollTween.current && goToSection(i),
+			const panels = gsap.utils.toArray(".panel");
+			panels.forEach((panel, i) => {
+				ScrollTrigger.create({
+					trigger: panel,
+					start: "top bottom",
+					end: "+=200%",
+					onToggle: (self) =>
+						self.isActive && !scrollTween.current && goToSection(i),
+				});
 			});
-		  });
-		  ScrollTrigger.create({
-			start: 0,
-			end: 'max',
-			snap: 1 / (panels.length - 1),
-		  });
+			ScrollTrigger.create({
+				start: 0,
+				end: "max",
+				snap: 1 / (panels.length - 1),
+			});
 		});
 		return () => ctx.revert();
-	  }, [completed]);
+	}, [completed]);
+
+	// in a useeffect if the window has a hash, and thep age is fully loaded scroll to that section
+	// in a useeffect if the page is fully loaded and the window has a hash scroll that section into view
+	useEffect(() => {
+		if (window.location.hash) {
+			const hash = window.location.hash;
+			const section = document.querySelector(hash);
+			setTimeout(() => {
+				console.log("could scroll");
+				if (section) {
+					console.log("scrolling to section");
+					section.scrollIntoView({ behavior: "smooth" });
+				}
+			}, 100);
+		}
+	}, []);
 
 	return (
 		<html lang="en">
@@ -79,7 +101,7 @@ export default function RootLayout({ children }) {
 					property="og:image"
 					content="https://parkelmcms.wpenginepowered.com/wp-content/uploads/2024/01/park-elm-at-century-plaza-open-graph-image.jpg"
 				/>
-				<TrackingScripts/>
+				<TrackingScripts />
 			</Head>
 			<StyledComponentsRegistry>
 				<body data-page={pathname}>
@@ -97,15 +119,15 @@ export default function RootLayout({ children }) {
 							/>
 						</a>
 					</noscript>
-						<Header />
-						<TransitionProvider>
+					<Header />
+					<TransitionProvider>
 						<TransitionComponent>
 							<main ref={main} className="panel">
 								{children}
 							</main>
 						</TransitionComponent>
-						</TransitionProvider>
-						<Footer />
+					</TransitionProvider>
+					<Footer />
 				</body>
 			</StyledComponentsRegistry>
 		</html>
