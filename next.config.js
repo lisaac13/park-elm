956ee656f8b9e878623c0b69a3fad76969c87b0b
lisaac13 <lisaac@abacusit.net>
2025-06-05 07:58:52 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,20 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	reactStrictMode: true,
-	experimental: {
-		serverActions: true,
-	},
-	compiler: {
-		styledComponents: true,
-	},
-	images: {
-		domains: ["cms.parkelmcenturyplaza.com"],
-	},
-	env: {
-		API_URL: "https://cms.parkelmcenturyplaza.com/graphql",
-		NEXT_RECAPTCHA_SITE_KEY: "6Le6NiIpAAAAAEjImqTyZkxGdTnrwbu-LW2olv2-",
-		NEXT_RECAPTCHA_SECRET_KEY: "6Le6NiIpAAAAAMc4TflVzFoDDRM_okciOKo6-uJL",
-	},
+  reactStrictMode: true,
+  experimental: {
+    serverActions: true,
+  },
+  compiler: {
+    styledComponents: true,
+  },
+  images: {
+    domains: ["cms.parkelmcenturyplaza.com"],
+    minimumCacheTTL: 2678400, // 31 days
+  },
+  env: {
+    API_URL: "https://cms.parkelmcenturyplaza.com/graphql",
+    NEXT_RECAPTCHA_SITE_KEY: "6Le6L6NlPpAAAABjImqTyZlxvGdTnrwbv-LW2olv2",
+    NEXT_RECAPTCHA_SECRET_KEY: "6Le6L6NlPpAAAAMC4fTLVzFoDORMJ_0kciOlKo6-uUL",
+  },
 };
 
 module.exports = nextConfig;
+
