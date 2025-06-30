a6885d3140073ad2e13d2ca3aaf71ec7e01ce2d7
lisaac13 <lisaac@abacusit.net>
2025-06-30 08:04:57 -0700

    Update verifyCaptcha.js

--- app/utils/verifyCaptcha.js
+++ app/utils/verifyCaptcha.js
@@ -1,15 +1,17 @@
 "use server";
+
 export const verifyCaptcha = async (verifyToken) => {
-	const res = await fetch("https://www.google.com/recaptcha/api/siteverify", {
-		method: "POST",
-		body: `secret=${process.env.NEXT_RECAPTCHA_SECRET_KEY}&response=${verifyToken}`,
-		headers: {
-			"Access-Control-Allow-Origin": "*",
-			"Content-Type": "application/x-www-form-urlencoded",
-		},
-	});
+  const res = await fetch("https://www.google.com/recaptcha/api/siteverify", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: new URLSearchParams({
+      secret: process.env.NEXT_RECAPTCHA_SECRET_KEY,
+      response: verifyToken,
+    }).toString(),
+  });
 
-	const data = await res.json();
-
-	return data;
+  const data = await res.json();
+  return data;
 };
